ef81969cc89319ae7de60a1c456e2118cf72b907
a000011 <nadkin.a@optimax.dev>
2024-10-07 14:55:45 -0700

    feat: add env for database

--- apps/telegram/src/data-source.ts
+++ apps/telegram/src/data-source.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { Config } from './models/all.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const AppDataSource = new DataSource({
 	type: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	password: 'postgres',
-	username:'postgres',
+	host: Config.DB_HOST,
+	port: Config.DB_PORT,
+	password: Config.DB_PASSWORD,
+	username: Config.DB_USERNAME,
 	database: 'RuleGuideBot',
 	synchronize: true,
 	logging: false,
--- apps/telegram/src/models/config/config.ts
+++ apps/telegram/src/models/config/config.ts
@@ -20,4 +20,36 @@ export class Config {
 
 		return process.env.ADMINS.split(',');
 	}
+
+	static get DB_HOST(): string {
+		if (!process.env.DB_HOST) {
+			throw new Error(getErrorEnvMessage('DB_HOST'));
+		}
+
+		return process.env.DB_HOST;
+	}
+
+	static get DB_PORT(): number {
+		if (!process.env.DB_PORT) {
+			throw new Error(getErrorEnvMessage('DB_PORT'));
+		}
+
+		return Number(process.env.DB_PORT);
+	}
+
+	static get DB_PASSWORD(): string {
+		if (!process.env.DB_PASSWORD) {
+			throw new Error(getErrorEnvMessage('DB_PASSWORD'));
+		}
+
+		return process.env.DB_PASSWORD;
+	}
+
+	static get DB_USERNAME(): string {
+		if (!process.env.DB_USERNAME) {
+			throw new Error(getErrorEnvMessage('DB_USERNAME'));
+		}
+
+		return process.env.DB_USERNAME;
+	}
 }
